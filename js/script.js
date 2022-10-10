814e7dec554e4b7764033702302480ee19130f0d
Reuben Ngururu <RNGURURU@safaricom.co.ke>
2022-10-10 23:52:34 +0300

    remove comment lines on js file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// var add = function(number1, number2) {
-//     return number1 + number2;
-//   };
-  
-//   var number1 = parseInt(prompt("Enter a number:"));
-//   var number2 = parseInt(prompt("Enter another number:"));
-//   var result = add(number1, number2);
-//   alert(result);
-
 const Days_Of_The_Week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const Male_Names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const Female_Names = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
