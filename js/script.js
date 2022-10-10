a9b63bd45b7b69b4d224f3b8cf6da9b6baec47c1
Reuben Ngururu <RNGURURU@safaricom.co.ke>
2022-10-10 12:15:19 +0300

    add modify js file by adding const

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@
 //   var number2 = parseInt(prompt("Enter another number:"));
 //   var result = add(number1, number2);
 //   alert(result);
+
+const Days_Of_The_Week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const Male_Names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+const Female_Names = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
